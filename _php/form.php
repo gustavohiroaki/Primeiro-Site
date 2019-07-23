d816482ecb15f5fd2e4997197e54cb7af80ae201
Gustavo Hiroaki <gustavo.hiroaki@csc.corp>
2019-07-23 08:14:32 -0300

    Primeiro Site no curso do Gustavo Guanabara

---
+++ _php/form.php
@@ -0,0 +1,18 @@
+﻿<?php
+echo "ola mundo";
+$nome = $_REQUEST['tNome'];
+$senha = $_REQUEST['tSenha'];
+$mail = $_REQUEST['tMail'];
+$sexo = $_REQUEST['cSexo'];
+$data = $_REQUEST['tData'];
+$rua = $_REQUEST['tRua'];
+$numero = $_REQUEST['tNum'];
+$cidade = $_REQUEST['tCid'];
+$urgencia = $_REQUEST['tUrg'];
+$mensagem = $_REQUEST['tMsg'];
+
+print_r($_REQUEST);
+
+echo $nome."|".$senha."|".$mail."|".$sexo."|".$data."|".$rua."|".$numero."|".$cidade."|".$urgencia."|".$mensagem;
+
+?>
